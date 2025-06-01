9ec5d576b90b52badc37882fa498677d58327fc3
Brainterminator <thebrainterminator@gmail.com>
2025-06-01 16:56:30 +0200

    -changed styling to create overflowing text

--- PosterCMS/Models/PosterModel.cs
+++ PosterCMS/Models/PosterModel.cs
@@ -1,15 +1,27 @@
-﻿namespace PosterCMS.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PosterCMS.Models;
 
 public class PosterModel
 {
     public int ID { get; set;}
-    public string? Title { get; set;} // Max 10
-    public string? Author { get; set;}
-    public DateTime? Date { get; set;}
-    public string? Sub1 { get; set;}
-    public string? Text1 { get; set;} // Max 870
-    public string? Sub2 { get; set;}
-    public string? Text2 { get; set;} // Max 560
-    public string? Sub3 { get; set;}
+
+    [MaxLength(20)]
+    public string? Title { get; set; }
+    
+    public string? Author { get; set; }
+    
+    public DateTime? Date { get; set; }
+    
+    public string? Sub1 { get; set; }
+    
+    public string? Text1 { get; set; } // Max 870
+    
+    public string? Sub2 { get; set; }
+    
+    public string? Text2 { get; set; } // Max 560
+    
+    public string? Sub3 { get; set; }
+    
     public string? Text3 { get; set;} // Max 560
 }
